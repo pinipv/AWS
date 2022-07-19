05ba4cfb7a17a82a397ed82d31c0577017c719ad
LLR-Laura <lauraledoruiz@gmail.com>
2022-07-19 20:48:40 +0200

    Update app.py

--- borrador_parte2/app.py
+++ borrador_parte2/app.py
@@ -13,13 +13,14 @@ def index():
     return render_template("homepage.html") # aquí ponemos el html con la homepage
 
 # Página insertar datos 
-@app.route("/data_input", methods=["POST"]) # ruta
+@app.route("/data_input/", methods=["POST"]) # ruta
 def data_input():
     return render_template("data_page.html") # aquí ponemos el html con la pg donde insertamos los datos para predicción
 
-#
-@app.route('/predict', methods=['GET', 'POST'])
+# Página con la predicción
+@app.route('/predict/', methods=['GET', 'POST'])
 def predict():
+
     # Usuario introduce los parámetros
     param_1 = request.form['parametro1']
     param_2 = request.form['parametro2']
@@ -27,13 +28,14 @@ def predict():
     param_4 = request.form['parametro4']
     param_5 = request.form['parametro5']
     param_6 = request.form['parametro6']
+
     # array para predicción
     arr = np.array([[param_1, param_2, param_3, param_4, param_5, param_6]])
-    arr = arr.astype(np.float64)
+
     # predicción
     pred = model.predict([arr])
 
     return render_template('predict.html', data=int(pred)) # aquí vamos a la página con la predicción
 
 if __name__ == "__main__":
-    app.run(debug=True) # debug = False antes de despliegue webservice !?
+    app.run(debug=True) # MUY IMPORTANTE!!!!! debug = False antes de despliegue a servidor público
