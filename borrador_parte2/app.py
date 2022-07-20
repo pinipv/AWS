bd2f4a897bc95ddc35cab08de41b4cc56e79ff8f
LLR-Laura <lauraledoruiz@gmail.com>
2022-07-20 14:28:36 +0200

    -

--- borrador_parte2/app.py
+++ borrador_parte2/app.py
@@ -2,29 +2,27 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
+# modelo de predicción
 def load_model():
-    with open(r'models\finished_model_arima_multiple.model', "rb") as archivo_entrada:
+    with open(r"\modelos\rf_model.pkl", "rb") as archivo_entrada:
         model = pickle.load(archivo_entrada)
         # print(list_models)
     return model
 
 app = Flask(__name__)
 
-# modelo de predicción
-model = pickle.load(open("!! Aquí poner nombre fichero .pkl con el modelo de predicción", 'rb'))
-
 # Homepage 
 @app.route("/", methods = ["GET"]) # ruta
 def index():
-    return render_template("homepage.html") # aquí ponemos el html con la homepage
+    return render_template("./manu/Templates/home.html") # aquí ponemos el html con la homepage
 
 # Página insertar datos 
-@app.route("/data_input/", methods=["POST"]) # ruta
+@app.route("/data_input/", methods=["GET"]) # ruta
 def data_input():
-    return render_template("data_page.html") # aquí ponemos el html con la pg donde insertamos los datos para predicción
+    return render_template("./manu/Templates/prediction.html") # aquí ponemos el html con la pg donde insertamos los datos para predicción
 
 # Página con la predicción
-@app.route('/data_input/predict/', methods=['GET', 'POST']) 
+@app.route('/data_input/predict/', methods=['POST']) 
 def predict():
         seasons={'spring':0, 
                 'summer':1, 
@@ -51,29 +49,7 @@ def predict():
         pred= model.predict(data.reshape(1,-1))
         #EL modelo devuelve una lista, recojo la primera y unica posicion y lo redondeo al mayor para que de un numero
         final_pred= round(pred[0]) 
-        return render_template('predict.html', data=int(final_pred))
-
-'''def predict():
-
-    # Parámetros del usuario
-    param_1 = request.form['parametro1']
-    param_2 = request.form['parametro2']
-    param_3 = request.form['parametro3']
-    param_4 = request.form['parametro4']
-    param_5 = request.form['parametro5']
-    param_6 = request.form['parametro6']
-
-    # array para predicción
-    arr = np.array([[param_1, param_2, param_3, param_4, param_5, param_6]])
-
-    # predicción
-    pred = model.predict([arr])
-
-    return render_template('predict.html', data=int(pred)) # aquí vamos a la página con la predicción
-'''
-
-    
-
-    
+        return render_template("./manu/Templates/after.html", data=int(final_pred))
+  
 if __name__ == "__main__":
     app.run(debug=True) # MUY IMPORTANTE!!!!! debug = False antes de despliegue a servidor público
